ad6a91908070edb5f506356998e41671671f3a9e
7sunarni <710720732@qq.com>
2024-02-18 10:08:32 +0800

    chore(deploy): install script

--- allInOne.sh
+++ allInOne.sh
@@ -9,11 +9,33 @@ export PATH=$PATH:/usr/local/go/bin
 
 curl 'https://codeload.github.com/EnnnOK/go-shadowsocks2/zip/refs/heads/master' -o temp.zip
 
-unzip temp.zip 
+unzip temp.zip
 
-cd go-shadowsocks2-master 
+mv go-shadowsocks2-master go-shadowsocks2
+
+rm temp.zip
+
+cd go-shadowsocks2
 
 /usr/local/go/bin/go build -o go-ss .
 
-export SHADOWSOCKS_SF_CAPACITY=1e6 SHADOWSOCKS_SF_FPR=1e-6 SHADOWSOCKS_SF_SLOT=10
-nohup ./go-ss -s ss://CHACHA20-IETF-POLY1305:${PASSWORD}@:58080 -verbose &
+cat > /lib/systemd/system/go-ss.service << eof
+[Unit]
+Description=My Miscellaneous Service
+After=network.target
+
+[Service]
+Type=simple
+Environment="SHADOWSOCKS_SF_CAPACITY=1e6"
+Environment="SHADOWSOCKS_SF_FPR=1e-6"
+Environment="SHADOWSOCKS_SF_SLOT=10"
+WorkingDirectory=/root/workspace/go-shadowsocks2
+ExecStart=/root/workspace/go-shadowsocks2/go-ss -s ss://CHACHA20-IETF-POLY1305:hello-ssh@:58080 -verbose
+Restart=on-failure # or always, on-abort, etc
+
+[Install]
+WantedBy=multi-user.target
+eof
+
+systemctl daemon-reload
+systemctl start go-ss.service
